81510b2ac058ba8c74e3fec98664b4c0dfe67920
shaozp <shaozepeng_2015@aliyun.com>
2020-06-24 16:00:53 +0800

    Initial Commit

---
+++ testtouch/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  testtouch
+//
+//  Created by 泽鹏邵 on 2020/6/24.
+//  Copyright © 2020 泽鹏邵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (readonly, strong) NSPersistentCloudKitContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ testtouch/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  testtouch
+//
+//  Created by 泽鹏邵 on 2020/6/24.
+//  Copyright © 2020 泽鹏邵. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
